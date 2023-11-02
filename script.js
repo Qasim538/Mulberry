011f58852353211968128daff1fbea6a12ffce2f
Qasim <mq12113@gmail.com>
2023-11-02 08:16:55 +0000

    JavaScript const

---
+++ script.js
@@ -0,0 +1,4 @@
+const carouselImages = document.querySelectorAll(".carousel-image");
+const leftArrow = document.getElementById("arrow-left");
+const rightArrow = document.getElementById("arrow-right");
+
